709f998b1e35cedb4c2a3973853123afacad37e1
Andrew Declercq <cowboypuck@gmail.com>
2014-03-01 13:09:26 -0600

    War generation working
    Note: Don't have any idea how this works.  Just followed downloaded directions.

---
+++ project/Build.scala
@@ -0,0 +1,25 @@
+import sbt._
+import Keys._
+import play.Project._
+import com.github.play2war.plugin._
+ 
+object ApplicationBuild extends Build {
+ 
+val appName = "y"
+val appVersion = "1.0-SNAPSHOT"
+ 
+val appDependencies = Seq(
+// Add your project dependencies here,
+javaCore,
+javaJdbc,
+javaEbean
+)
+ 
+val main = play.Project(appName, appVersion, appDependencies)
+.settings(Play2WarPlugin.play2WarSettings: _*)
+ 
+.settings(
+Play2WarKeys.servletVersion := "3.0"
+)
+// Add your own project settings here
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,3 +6,8 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.1")
+
+// War plugin
+resolvers += "Play2war plugins release" at "http://repository-play-war.forge.cloudbees.com/release/"
+
+addSbtPlugin("com.github.play2war" % "play2-war-plugin" % "1.2-beta2")
