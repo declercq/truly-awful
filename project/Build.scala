068ddd679237789487d73a5c76b669e2f4f97603
Andrew Declercq <cowboypuck@gmail.com>
2014-03-04 23:16:12 -0600

    Added mysql connector dependency

--- project/Build.scala
+++ project/Build.scala
@@ -5,21 +5,22 @@ import com.github.play2war.plugin._
  
 object ApplicationBuild extends Build {
  
-val appName = "y"
-val appVersion = "1.0-SNAPSHOT"
+    val appName = "y"
+    val appVersion = "1.0-SNAPSHOT"
  
-val appDependencies = Seq(
-// Add your project dependencies here,
-javaCore,
-javaJdbc,
-javaEbean
-)
+    val appDependencies = Seq(
+        // Add your project dependencies here,
+        javaCore,
+        javaJdbc,
+        javaEbean,
+        "mysql" % "mysql-connector-java" % "5.1.29"
+    )
  
-val main = play.Project(appName, appVersion, appDependencies)
-.settings(Play2WarPlugin.play2WarSettings: _*)
+    val main = play.Project(appName, appVersion, appDependencies)
+    .settings(Play2WarPlugin.play2WarSettings: _*)
  
-.settings(
-Play2WarKeys.servletVersion := "3.0"
-)
-// Add your own project settings here
+    .settings(
+    Play2WarKeys.servletVersion := "3.0"
+    )
+    // Add your own project settings here
 }
